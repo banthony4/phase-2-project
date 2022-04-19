7f4b54ec23845a3dcc50a677cf073f737816c46e
Ben <banthony446@gmail.com>
2022-04-19 11:22:39 -0400

    filters

--- sneaker-app/src/Components/SneakersContainer.js
+++ sneaker-app/src/Components/SneakersContainer.js
@@ -8,6 +8,21 @@ function SneakerContainer({ sneakers }) {
   }
   return (
     <div>
+      <form>
+        <label>Search:</label>
+        <input tpye='text' id='name' name='name' ></input>
+        <label for='brand'>Select Brand:</label>
+        <select id='brand' name='brand'>
+          <option value='nike'>Nike</option>
+          <option value='adidas'>Adidas</option>
+          <option value='converse'>Converse</option>
+          <option value='jordan'>Air Jordan</option>
+          <option value='yeezy'>Yeezy</option>
+          <option value='reebok'>Reebok</option>
+        </select>
+        <input type='checkbox' id='mens' name='mens'>Mens</input>
+        <input type='checkbox' id='womens' name='womens'>Womens</input>
+      </form>
       <ul className="SneakerCollection">
         {RenderSneakers()}
       </ul>
